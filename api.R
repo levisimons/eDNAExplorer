b47ee04a2832c031d8143973d52e8e83ea05224e
Ariel Levi Simons <levisimons@gmail.com>
2023-03-23 20:25:29 -0700

    Update api.R

--- api.R
+++ api.R
@@ -1,4 +1,5 @@
 source("eDNAExplorer_Alpha.R")
+source("eDNAExplorer_Beta.R")
 #' @param ProjectID
 #' @param First_Date
 #' @param Last_Date
@@ -10,6 +11,8 @@ source("eDNAExplorer_Alpha.R")
 #' @param EnvironmentalParameter
 #' @param AlphaDiversity
 #' @get /alpha
-function(ProjectID,First_Date,Last_Date,Marker,Num_Mismatch,TaxonomicRank,CountThreshold,FilterThreshold,EnvironmentalParameter,AlphaDiversity){
+#' @get /beta
+function(ProjectID,First_Date,Last_Date,Marker,Num_Mismatch,TaxonomicRank,CountThreshold,FilterThreshold,EnvironmentalParameter,AlphaDiversity,BetaDiversity){
   alpha(ProjectID,First_Date,Last_Date,Marker,Num_Mismatch,TaxonomicRank,CountThreshold,FilterThreshold,EnvironmentalParameter,AlphaDiversity)
+  beta(ProjectID,First_Date,Last_Date,Marker,Num_Mismatch,TaxonomicRank,CountThreshold,FilterThreshold,EnvironmentalParameter,BetaDiversity)
 }
