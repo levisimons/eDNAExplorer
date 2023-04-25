bf3c465e51dd5af69bbf25b545beb0373a932f5d
Ariel Levi Simons <levisimons@gmail.com>
2023-04-25 10:39:43 -0700

    Update eDNAExplorer_Alpha.R

--- eDNAExplorer_Alpha.R
+++ eDNAExplorer_Alpha.R
@@ -19,8 +19,13 @@ require(DBI)
 require(RPostgreSQL)
 require(digest)
 
-Sys.setenv("AWS_ACCESS_KEY_ID" = "",
-           "AWS_SECRET_ACCESS_KEY" = "")
+Sys.setenv("AWS_ACCESS_KEY_ID" = Sys.getenv("AWS_ACCESS_KEY_ID"),
+           "AWS_SECRET_ACCESS_KEY" = Sys.getenv("AWS_SECRET_ACCESS_KEY"))
+db_host <- Sys.getenv("db_host")
+db_port <- Sys.getenv("db_port")
+db_name <- Sys.getenv("db_name")
+db_user <- Sys.getenv("db_user")
+db_pass <- Sys.getenv("db_pass")
 
 #* Echo the parameter that was sent in
 #* @param ProjectID:string
@@ -54,11 +59,6 @@ alpha <- function(ProjectID,First_Date,Last_Date,Marker,Num_Mismatch,TaxonomicRa
   TaxonomicRanks <- c("superkingdom","kingdom","phylum","class","order","family","genus","species")
   
   #Establish sql connection
-  db_host <- ""
-  db_port <- 
-  db_name <- ""
-  db_user <- ""
-  db_pass <- ""
   Database_Driver <- dbDriver("PostgreSQL")
   sapply(dbListConnections(Database_Driver), dbDisconnect)
   
