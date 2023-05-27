e18045e6ed3fa06b4e806e858910780ce8d87edc
Ariel Levi Simons <levisimons@gmail.com>
2023-05-27 12:20:16 -0700

    Update eDNAExplorer_Prevalence.R

--- eDNAExplorer_Prevalence.R
+++ eDNAExplorer_Prevalence.R
@@ -49,14 +49,14 @@ prevalence <- function(ProjectID,First_Date,Last_Date,Marker,Num_Mismatch,Taxono
   Metadata <- tbl(con,"TronkoMetadata")
   Keep_Vars <- c(CategoricalVariables,ContinuousVariables,FieldVars)[c(CategoricalVariables,ContinuousVariables,FieldVars) %in% dbListFields(con,"TronkoMetadata")]
   Metadata <- Metadata %>% filter(sample_date >= First_Date & sample_date <= Last_Date) %>%
-    filter(`ProjectID` == ProjectID) %>% filter(!is.na(latitude) & !is.na(longitude)) %>% select(Keep_Vars)
+    filter(ProjectID == ProjectID) %>% filter(!is.na(latitude) & !is.na(longitude)) %>% select(Keep_Vars)
   Metadata <- as.data.frame(Metadata)
   sapply(dbListConnections(Database_Driver), dbDisconnect)
   
   #Read in Tronko output and filter it.
   con <- dbConnect(Database_Driver,host = db_host,port = db_port,dbname = db_name,user = db_user,password = db_pass)
   TronkoInput <- tbl(con,"TronkoOutput")
-  TronkoInput <- TronkoInput %>% filter(`ProjectID` == ProjectID) %>% filter(Primer == Marker) %>% 
+  TronkoInput <- TronkoInput %>% filter(ProjectID == ProjectID) %>% filter(Primer == Marker) %>% 
     filter(Mismatch <= Num_Mismatch & !is.na(Mismatch)) %>% filter(!is.na(!!sym(TaxonomicRank))) %>%
     group_by(SampleID) %>% filter(n() > CountThreshold) %>% 
     select(SampleID,kingdom,TaxonomicRank)
