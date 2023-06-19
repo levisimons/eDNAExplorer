c849d0ad43da71fedc79fb876678a17b6d51e962
Ariel Levi Simons <levisimons@gmail.com>
2023-06-19 16:04:40 -0700

    Update eDNAExplorer_Map_Metabarcoding.R

--- eDNAExplorer_Map_Metabarcoding.R
+++ eDNAExplorer_Map_Metabarcoding.R
@@ -105,5 +105,8 @@ map <- function(Marker,Taxon,TaxonomicRank,Num_Mismatch,CountThreshold,FilterThr
   Taxon_Map_Data <- rbind(TaxonMap,Metadata_Filtered)
   #Return results
   Taxon_Map_Data <- jsonlite::toJSON(Taxon_Map_Data)
-  return(Taxon_Map_Data)
+  filename <- paste("Map_Metabarcoding_Marker_",Marker,"_Taxon_",Taxon,"_Rank_",TaxonomicRank,"_Mismatch_",Num_Mismatch,"_CountThreshold_",CountThreshold,"_AbundanceThreshold_",format(sample_FilterThreshold,scientific=F),".json",,sep="")
+  write(Taxon_Map_Data,filename)
+  system(paste("aws s3 cp ",filename," s3://ednaexplorer/projects/",ProjectID,"/plots/",filename," --endpoint-url https://js2.jetstream-cloud.org:8001/",sep=""),intern=TRUE)
+  system(paste("rm ",filename,sep=""))
 }
