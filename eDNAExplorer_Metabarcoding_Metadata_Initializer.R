6faa4b459802a930131dfd777c1c804bf185af87
Ariel Levi Simons <levisimons@gmail.com>
2023-07-03 16:56:51 -0700

    Update eDNAExplorer_Metabarcoding_Metadata_Initializer.R

--- eDNAExplorer_Metabarcoding_Metadata_Initializer.R
+++ eDNAExplorer_Metabarcoding_Metadata_Initializer.R
@@ -49,7 +49,7 @@ for(csv_file in unique(Project_Scan$Filename)){
   if(length(grep("Marker 1",Metadata_Initial))==1){
     #Read in qPCR project data.
     Metadata_Initial <- system(paste("aws s3 cp s3://ednaexplorer/",csv_file," - --endpoint-url https://js2.jetstream-cloud.org:8001/",sep=""),intern=TRUE)
-    Metadata_Initial <- gsub("[\r\n]", "", Metadata_Initial))
+    Metadata_Initial <- gsub("[\r\n]", "", Metadata_Initial)
     Metadata_Initial <- read.table(text = Metadata_Initial,header=FALSE, sep=",",as.is=T,skip=0,fill=TRUE,check.names=FALSE,quote = "\"", encoding = "UTF-8",na = c("", "NA", "N/A"))
     colnames(Metadata_Initial) <- Metadata_Initial[5,]
     Metadata_Initial <- Metadata_Initial[6:nrow(Metadata_Initial),]
