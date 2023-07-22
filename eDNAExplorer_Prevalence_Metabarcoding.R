8c8832be6f7f7ad9c4a30b4d40e4fdd30eb65db3
Ariel Levi Simons <levisimons@gmail.com>
2023-07-22 09:15:09 -0700

    Update eDNAExplorer_Prevalence_Metabarcoding.R

--- eDNAExplorer_Prevalence_Metabarcoding.R
+++ eDNAExplorer_Prevalence_Metabarcoding.R
@@ -65,6 +65,14 @@ prevalence <- function(ProjectID,First_Date,Last_Date,Marker,Num_Mismatch,Taxono
     SpeciesList_df <- as.data.frame(SpeciesList_df)
   }
   
+  #Read in metadata and filter it.
+  Metadata <- tbl(con,"TronkoMetadata")
+  Keep_Vars <- c(CategoricalVariables,ContinuousVariables,FieldVars)[c(CategoricalVariables,ContinuousVariables,FieldVars) %in% dbListFields(con,"TronkoMetadata")]
+  Metadata <- Metadata %>% filter(sample_date >= First_Date & sample_date <= Last_Date) %>%
+    filter(ProjectID == ProjectID) %>% filter(!is.na(latitude) & !is.na(longitude)) %>% select(site,sample_id,sample_date,fastqid)
+  Metadata <- as.data.frame(Metadata)
+  Metadata$fastqid <- gsub("_","-",Metadata$fastqid)
+  
   #Read in Tronko output and filter it.
   TronkoInput <- tbl(con,"TronkoOutput")
   TronkoInput <- TronkoInput %>% filter(ProjectID == Project_ID) %>% filter(Primer == Marker) %>% 
@@ -72,6 +80,8 @@ prevalence <- function(ProjectID,First_Date,Last_Date,Marker,Num_Mismatch,Taxono
     group_by(SampleID) %>% filter(n() > CountThreshold) %>% 
     select(SampleID,TaxonomicRanks)
   TronkoDB <- as.data.frame(TronkoInput)
+  if(SelectedSpeciesList != "None"){TronkoDB <- TronkoDB[TronkoDB$SampleID %in% unique(na.omit(Metadata$fastqid)) & TronkoDB$species %in% SpeciesList_df$name,]}
+  if(SelectedSpeciesList == "None"){TronkoDB <- TronkoDB[TronkoDB$SampleID %in% unique(na.omit(Metadata$fastqid)),]}
   
   #Read in Taxonomy output and filter it.
   TaxonomyInput <- tbl(con,"Taxonomy")
