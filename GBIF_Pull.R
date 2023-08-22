3f97093a954e4c800d4f8369f25f5f32f27afaa3
Ariel Levi Simons <levisimons@gmail.com>
2023-08-22 13:21:33 -0700

    Update GBIF_Pull.R

--- GBIF_Pull.R
+++ GBIF_Pull.R
@@ -1,5 +1,7 @@
 #This script downloads the most recent copy of the GBIF database to a local drive.
 require(gbifdb)
 require(dplyr)
+readRenviron(".env")
+gbif_dir <- Sys.getenv("GBIF_HOME")
 gbif_download() #Run monthly to create GBIF mirror
 #By default, this will download to the dir given by gbif_dir() and can be changed to a different path.
