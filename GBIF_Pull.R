a0be41bf028ec01c4e4e760bb83a5305d374f9f1
Ariel Levi Simons <levisimons@gmail.com>
2023-08-24 08:26:35 -0700

    Update GBIF_Pull.R

--- GBIF_Pull.R
+++ GBIF_Pull.R
@@ -3,5 +3,6 @@ require(gbifdb)
 require(dplyr)
 readRenviron(".env")
 gbif_dir <- Sys.getenv("GBIF_HOME")
+system(paste("sudo rm -rf ",gbif_dir,"/occurrence",sep=""))
 gbif_download(dir=gbif_dir) #Run monthly to create GBIF mirror
 #By default, this will download to the dir given by gbif_dir() and can be changed to a different path.
