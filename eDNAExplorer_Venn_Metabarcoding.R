dac70365365b931b0f7cdfa13362f5d4a5045884
Ariel Levi Simons <levisimons@gmail.com>
2023-08-28 14:02:37 -0700

    Update eDNAExplorer_Venn_Metabarcoding.R

--- eDNAExplorer_Venn_Metabarcoding.R
+++ eDNAExplorer_Venn_Metabarcoding.R
@@ -15,6 +15,7 @@ require(data.table)
 require(DBI)
 require(RPostgreSQL)
 require(digest)
+require(aws.s3)
 
 # Fetch project ID early so we can use it for error output when possible.
 ProjectID <- args[1]
