590701c85f4c2b848230d9b168e6df02afd95dac
Ariel Levi Simons <levisimons@gmail.com>
2023-08-28 16:22:43 -0700

    Update install.R

--- install.R
+++ install.R
@@ -1,2 +1,3 @@
-install.packages(c("aws.s3", "tidyr", "dplyr", "vegan", "ggplot2", "lubridate", "htmlwidgets", "plotly", "jsonlite", "BiocManager", "plumber","ggVennDiagram"))
+install.packages(c("tidyr", "dplyr", "vegan", "ggplot2", "lubridate", "htmlwidgets", "plotly", "jsonlite", "BiocManager","zoo","ggVennDiagram"))
+
 BiocManager::install("phyloseq")
