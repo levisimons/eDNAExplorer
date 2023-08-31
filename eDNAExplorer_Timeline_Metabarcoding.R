87278b8e4e496336254908a5629d6f8b8e509c0f
Ariel Levi Simons <levisimons@gmail.com>
2023-08-31 08:48:25 -0700

    Update eDNAExplorer_Timeline_Metabarcoding.R

--- eDNAExplorer_Timeline_Metabarcoding.R
+++ eDNAExplorer_Timeline_Metabarcoding.R
@@ -32,9 +32,9 @@ process_error <- function(e, filename = "error.json") {
   } else {
     paste("s3://ednaexplorer/projects/", ProjectID, "/plots/", filename, " --endpoint-url https://js2.jetstream-cloud.org:8001/", sep = "")
   }
+  
   system(paste("aws s3 cp ", filename, " ", s3_path, sep = ""), intern = TRUE)
   system(paste("rm ",filename,sep=""))
-  sapply(dbListConnections(Database_Driver), dbDisconnect)
   stop(error_message)
 }
 
@@ -70,15 +70,20 @@ tryCatch(
       CountThreshold <- args[5]
       FilterThreshold <- args[6]
       Taxon_name <- args[7]
+      
+      Project_ID <- as.character(ProjectID)
+      Taxon <- as.character(Taxon_name)
+      #Get GBIF taxonomy key for taxon.
+      Taxon_GBIF <- name_backbone(name=Taxon,rank=TaxonomicRank)$usageKey
+      #Ensure numeric values.
+      Num_Mismatch <- as.numeric(Num_Mismatch)
+      CountThreshold <- as.numeric(CountThreshold)
+      FilterThreshold <- as.numeric(FilterThreshold)
+      
+      filename <- paste("Timeline_Metabarcoding_Marker_",Marker,"_Taxon_",Taxon,"_Rank_",TaxonomicRank,"_Mismatch_",Num_Mismatch,"_CountThreshold_",CountThreshold,"_AbundanceThreshold_",format(FilterThreshold,scientific=F),".json",sep="")
+      filename <- tolower(filename)
+      filename <- gsub(" ","_",filename)
     }
-    Project_ID <- as.character(ProjectID)
-    Taxon <- as.character(Taxon_name)
-    #Get GBIF taxonomy key for taxon.
-    Taxon_GBIF <- name_backbone(name=Taxon,rank=TaxonomicRank)$usageKey
-    #Ensure numeric values.
-    Num_Mismatch <- as.numeric(Num_Mismatch)
-    CountThreshold <- as.numeric(CountThreshold)
-    FilterThreshold <- as.numeric(FilterThreshold)
   },
   error = function(e) {
     process_error(e)
@@ -88,6 +93,12 @@ tryCatch(
 # Generate the output filename for cached plots.
 tryCatch(
   {
+    # Output a blank json output for plots as a default.  This gets overwritten is actual plot material exists.
+    data_to_write <- list(generating = TRUE, lastRanAt = Sys.time())
+    write(toJSON(data_to_write), filename)
+    system(paste("aws s3 cp ", filename, " s3://ednaexplorer/projects/", Project_ID, "/plots/", filename, " --endpoint-url https://js2.jetstream-cloud.org:8001/", sep = ""), intern = TRUE)
+    system(paste("rm ", filename, sep = ""))
+    
     #Establish sql connection
     Database_Driver <- dbDriver("PostgreSQL")
     sapply(dbListConnections(Database_Driver), dbDisconnect)
@@ -117,7 +128,7 @@ tryCatch(
     TronkoFile_tmp <- paste(Marker,"_timeline_",UUIDgenerate(),".csv",sep="")
     system(paste("aws s3 cp s3://ednaexplorer/tronko_output/", Project_ID, "/", TronkoFile, " ", TronkoFile_tmp, " --endpoint-url https://js2.jetstream-cloud.org:8001/", sep = ""))
     # Select relevant columns in bash (SampleID, taxonomic ranks, Mismatch)
-    SubsetFile <- paste("subset_map_",UUIDgenerate(),".csv",sep="")
+    SubsetFile <- paste("subset_timeline_",UUIDgenerate(),".csv",sep="")
     awk_command <- sprintf("awk -F, 'BEGIN {OFS=\",\"} NR == 1 {for (i=1; i<=NF; i++) col[$i] = i} {print $col[\"SampleID\"], $col[\"superkingdom\"], $col[\"kingdom\"], $col[\"phylum\"], $col[\"class\"], $col[\"order\"], $col[\"family\"], $col[\"genus\"], $col[\"species\"], $col[\"Mismatch\"]}' %s > %s",TronkoFile_tmp, SubsetFile)
     system(awk_command, intern = TRUE)
     # Filter on the number of mismatches.  Remove entries with NA for mismatches and for the selected taxonomic rank.
@@ -131,18 +142,21 @@ tryCatch(
       select(SampleID, kingdom, phylum, class, order, family, genus, species)
     TronkoDB <- as.data.frame(TronkoInput)
     TronkoDB <- TronkoDB[TronkoDB[,TaxonomicRank]==Taxon,]
-    
-    #Get samples where taxon occurs and meets Tronko filters.
-    TaxonDB <- TronkoDB[!duplicated(TronkoDB),]
-    TaxonDB$SampleID <- gsub("-","_",TaxonDB$SampleID)
     system(paste("rm ",TronkoFile_tmp,sep=""))
-    system("rm ",SubsetFile,sep="")
-    taxon_samples <- unique(TaxonDB$SampleID)
-    taxon_projects <- ProjectID
+    system(paste("rm ",SubsetFile,sep=""))
     
     #Get samples where taxon occurs and meets Tronko filters.
-    taxon_samples <- unique(TaxonDB$SampleID)
-    taxon_projects <- ProjectID
+    if(nrow(TronkoDB)>=1){
+      TaxonDB <- TronkoDB[!duplicated(TronkoDB),]
+      TaxonDB$SampleID <- gsub("-","_",TaxonDB$SampleID)
+      #Get samples where taxon occurs and meets Tronko filters.
+      taxon_samples <- unique(TaxonDB$SampleID)
+      taxon_projects <- ProjectID
+    }
+    if(nrow(TronkoDB)<1){
+      taxon_samples <- c()
+      taxon_projects <- c()
+    }
     
     #Read in metadata and filter it.
     con <- dbConnect(Database_Driver,host = db_host,port = db_port,dbname = db_name,user = db_user,password = db_pass)
@@ -156,10 +170,16 @@ tryCatch(
     sapply(dbListConnections(Database_Driver), dbDisconnect)
     
     #Get projects and years where taxon was detected via eDNA.
-    eDNA <- Metadata_Filtered[,c("year","projectid")]
-    colnames(eDNA) <- c("year","eDNA_projects")
-    eDNA <- eDNA[complete.cases(eDNA),]
-    eDNA <- eDNA[!duplicated(eDNA),]
+    if(nrow(Metadata_Filtered)>=1){
+      eDNA <- Metadata_Filtered[,c("year","projectid")]
+      colnames(eDNA) <- c("year","eDNA_projects")
+      eDNA <- eDNA[complete.cases(eDNA),]
+      eDNA <- eDNA[!duplicated(eDNA),]
+    }
+    if(nrow(Metadata_Filtered)<1){
+      eDNA <- data.frame(matrix(nrow=1,ncol=2))
+      colnames(eDNA) <- c("year","projectid")
+    }
     
     #Merge eDNA and GBIF timeline data.
     Timeline <- merge(Taxa_Time,eDNA,by="year",all=T)
