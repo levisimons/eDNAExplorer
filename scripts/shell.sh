491023f43b31d54d72d32bd750163d09e2c41b21
Jim Jeffers <jim@sumocreations.com>
2023-09-12 22:01:16 -0700

    Updated run scripts to v3

--- scripts/shell.sh
+++ scripts/shell.sh
@@ -1,2 +1,2 @@
 #bin/bash
-docker run --platform linux/amd64 --entrypoint /bin/bash -v $PWD:/project -it jimjeffers/edna-explorer:v2
+docker run --platform linux/amd64 --entrypoint /bin/bash -v $PWD:/project -it jimjeffers/edna-explorer:v3
