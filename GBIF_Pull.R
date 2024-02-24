9a0217f15b439c68c47257efde16886f7aa567cb
Jim Jeffers <jim@sumocreations.com>
2024-02-23 18:21:10 -0800

    GBIF Pull script now logs errors via sentry. No longer uses sudo.

--- GBIF_Pull.R
+++ GBIF_Pull.R
@@ -1,8 +1,20 @@
-#This script downloads the most recent copy of the GBIF database to a local drive.
+# This script downloads the most recent copy of the GBIF database to a local drive.
 require(gbifdb)
 require(dplyr)
+library(sentryR)
 readRenviron(".env")
+
+configure_sentry(
+    dsn = Sys.getenv("SENTRY_DSN"),
+    app_name = "r-report-service", app_version = "1.1.0",
+    environment = Sys.getenv("APP_ENV"),
+    runtime = NULL
+)
+cat("Configured Sentry: ", Sys.getenv("SENTRY_DSN"), "\n")
+
 gbif_dir <- Sys.getenv("GBIF_HOME")
-system(paste("sudo rm -rf ",gbif_dir,"/occurrence",sep=""))
-gbif_download(dir=gbif_dir) #Run monthly to create GBIF mirror
-#By default, this will download to the dir given by gbif_dir() and can be changed to a different path.
+cat("GBIF Home: ", gbif_dir, "\n")
+cat("Removing old GBIF data\n")
+system(paste("rm -rf ", gbif_dir, "/occurrence", sep = ""))
+cat("Downloading new GBIF data\n")
+gbif_download(dir = gbif_dir) # Run monthly to create GBIF mirror
